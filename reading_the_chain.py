0cbac51b90e8fe03a172dae469dd67ad2c5c8931
mcpars <157234335+mcpars@users.noreply.github.com>
2026-03-03 12:43:45 -0500

    Update reading_the_chain.py

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -11,105 +11,84 @@ from web3.providers.rpc import HTTPProvider
 # infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
 
 def connect_to_eth():
-	url = "https://mainnet.infura.io/v3/d59991e8df07469796a0e81d0c148b83"  # FILL THIS IN
-	w3 = Web3(HTTPProvider(url))
-	assert w3.is_connected(), f"Failed to connect to provider at {url}"
-  	return w3
+	# TODO insert your code for this method from last week's assignment
+	return w3
 
 
 def connect_with_middleware(contract_json):
-    with open(contract_json, "r") as f:
-        d = json.load(f)
-        d = d["bsc"]
-        address = d["address"]
-        abi = d["abi"]
-
-    # The first section will be the same as "connect_to_eth()" but with a BNB url
-    bnb_url = "https://data-seed-prebsc-1-s1.binance.org:8545/"
-    w3 = Web3(HTTPProvider(bnb_url))
-    assert w3.is_connected(), f"Failed to connect to provider at {bnb_url}"
-
-    # Inject middleware (required for BNB/BSC testnet POA chains)
-    w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
-
-    checksum_address = Web3.to_checksum_address(address)
-
-    # Create the contract object
-    contract = w3.eth.contract(address=checksum_address, abi=abi)
-
-    return w3, contract
+	# TODO insert your code for this method from last week's assignment
+	return w3, contract
 
 
-        def is_ordered_block(w3, block_num):
-        	"""
-          	Takes a block number
-            	Returns a boolean that tells whether all the transactions in the block are ordered by priority fee
+def is_ordered_block(w3, block_num):
+	"""
+	Takes a block number
+	Returns a boolean that tells whether all the transactions in the block are ordered by priority fee
 
-              	Before EIP-1559, a block is ordered if and only if all transactions are sorted in decreasing order of the gasPrice field
+	Before EIP-1559, a block is ordered if and only if all transactions are sorted in decreasing order of the gasPrice field
 
-                	After EIP-1559, there are two types of transactions
-                  		*Type 0* The priority fee is tx.gasPrice - block.baseFeePerGas
-                      		*Type 2* The priority fee is min( tx.maxPriorityFeePerGas, tx.maxFeePerGas - block.baseFeePerGas )
+	After EIP-1559, there are two types of transactions
+		*Type 0* The priority fee is tx.gasPrice - block.baseFeePerGas
+		*Type 2* The priority fee is min( tx.maxPriorityFeePerGas, tx.maxFeePerGas - block.baseFeePerGas )
 
-                          	Conveniently, most type 2 transactions set the gasPrice field to be min( tx.maxPriorityFeePerGas + block.baseFeePerGas, tx.maxFeePerGas )
-                            	"""
-                              	block = w3.eth.get_block(block_num, full_transactions=True)
-                                	ordered = False
+	Conveniently, most type 2 transactions set the gasPrice field to be min( tx.maxPriorityFeePerGas + block.baseFeePerGas, tx.maxFeePerGas )
+	"""
+	block = w3.eth.get_block(block_num, full_transactions=True)
+	ordered = False
 
-                                  	# TODO YOUR CODE HERE
+	# TODO YOUR CODE HERE
 
-                                    	return ordered
-
-
-                                      def get_contract_values(contract, admin_address, owner_address):
-                                      	"""
-                                        	Takes a contract object, and two addresses (as strings) to be used for calling
-                                          	the contract to check current on chain values.
-                                            	The provided "default_admin_role" is the correctly formatted solidity default
-                                              	admin value to use when checking with the contract
-                                                	To complete this method you need to make three calls to the contract to get:
-                                                  	  onchain_root: Get and return the merkleRoot from the provided contract
-                                                      	  has_role: Verify that the address "admin_address" has the role "default_admin_role" return True/False
-                                                          	  prime: Call the contract to get and return the prime owned by "owner_address"
-
-                                                              	check on available contract functions and transactions on the block explorer at
-                                                                	https://testnet.bscscan.com/address/0xaA7CAaDA823300D18D3c43f65569a47e78220073
-                                                                  	"""
-                                                                    	default_admin_role = int.to_bytes(0, 32, byteorder="big")
-
-                                                                      	# TODO complete the following lines by performing contract calls
-                                                                        	onchain_root = 0  # Get and return the merkleRoot from the provided contract
-                                                                          	has_role = 0  # Check the contract to see if the address "admin_address" has the role "default_admin_role"
-                                                                            	prime = 0  # Call the contract to get the prime owned by "owner_address"
-
-                                                                              	return onchain_root, has_role, prime
+	return ordered
 
 
-                                                                                """
-                                                                                	This might be useful for testing (main is not run by the grader feel free to change 
-                                                                                  	this code anyway that is helpful)
-                                                                                    """
-                                                                                    if __name__ == "__main__":
-                                                                                    	# These are addresses associated with the Merkle contract (check on contract
-                                                                                      	# functions and transactions on the block explorer at
-                                                                                        	# https://testnet.bscscan.com/address/0xaA7CAaDA823300D18D3c43f65569a47e78220073
-                                                                                          	admin_address = "0xAC55e7d73A792fE1A9e051BDF4A010c33962809A"
-                                                                                            	owner_address = "0x793A37a85964D96ACD6368777c7C7050F05b11dE"
-                                                                                              	contract_file = "contract_info.json"
+def get_contract_values(contract, admin_address, owner_address):
+	"""
+	Takes a contract object, and two addresses (as strings) to be used for calling
+	the contract to check current on chain values.
+	The provided "default_admin_role" is the correctly formatted solidity default
+	admin value to use when checking with the contract
+	To complete this method you need to make three calls to the contract to get:
+	  onchain_root: Get and return the merkleRoot from the provided contract
+	  has_role: Verify that the address "admin_address" has the role "default_admin_role" return True/False
+	  prime: Call the contract to get and return the prime owned by "owner_address"
 
-                                                                                                	eth_w3 = connect_to_eth()
-                                                                                                  	cont_w3, contract = connect_with_middleware(contract_file)
+	check on available contract functions and transactions on the block explorer at
+	https://testnet.bscscan.com/address/0xaA7CAaDA823300D18D3c43f65569a47e78220073
+	"""
+	default_admin_role = int.to_bytes(0, 32, byteorder="big")
 
-                                                                                                    	latest_block = eth_w3.eth.get_block_number()
-                                                                                                      	london_hard_fork_block_num = 12965000
-                                                                                                        	assert latest_block > london_hard_fork_block_num, f"Error: the chain never got past the London Hard Fork"
+	# TODO complete the following lines by performing contract calls
+	onchain_root = 0  # Get and return the merkleRoot from the provided contract
+	has_role = 0  # Check the contract to see if the address "admin_address" has the role "default_admin_role"
+	prime = 0  # Call the contract to get the prime owned by "owner_address"
 
-                                                                                                          	n = 5
-                                                                                                            	for _ in range(n):
-                                                                                                              		block_num = random.randint(1, latest_block)
-                                                                                                                  		ordered = is_ordered_block(block_num)
-                                                                                                                      		if ordered:
-                                                                                                                          			print(f"Block {block_num} is ordered")
-                                                                                                                                		else:
-                                                                                                                                    			print(f"Block {block_num} is not ordered")
-                                                                                                                                          
+	return onchain_root, has_role, prime
+
+
+"""
+	This might be useful for testing (main is not run by the grader feel free to change 
+	this code anyway that is helpful)
+"""
+if __name__ == "__main__":
+	# These are addresses associated with the Merkle contract (check on contract
+	# functions and transactions on the block explorer at
+	# https://testnet.bscscan.com/address/0xaA7CAaDA823300D18D3c43f65569a47e78220073
+	admin_address = "0xAC55e7d73A792fE1A9e051BDF4A010c33962809A"
+	owner_address = "0x793A37a85964D96ACD6368777c7C7050F05b11dE"
+	contract_file = "contract_info.json"
+
+	eth_w3 = connect_to_eth()
+	cont_w3, contract = connect_with_middleware(contract_file)
+
+	latest_block = eth_w3.eth.get_block_number()
+	london_hard_fork_block_num = 12965000
+	assert latest_block > london_hard_fork_block_num, f"Error: the chain never got past the London Hard Fork"
+
+	n = 5
+	for _ in range(n):
+		block_num = random.randint(1, latest_block)
+		ordered = is_ordered_block(block_num)
+		if ordered:
+			print(f"Block {block_num} is ordered")
+		else:
+			print(f"Block {block_num} is not ordered")
